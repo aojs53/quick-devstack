36f2894fb829d77a69407ef6b1ecef73fadc486c
Akihiro Motoki <amotoki@gmail.com>
2014-11-17 01:48:58 +0900

    Move devstack to $HOME and create VG for cinder

--- stack01/prepare.sh
+++ stack01/prepare.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 TOP_DIR=$(cd $(dirname "$0") && pwd)
-DEVSTACK_DIR=$TOP_DIR/devstack
+DEVSTACK_DIR=$HOME/devstack
 
 git clone -b stable/juno git://git.openstack.org/openstack-dev/devstack.git $DEVSTACK_DIR
 cp local.conf $DEVSTACK_DIR/local.conf
--- stack02/prepare.sh
+++ stack02/prepare.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 TOP_DIR=$(cd $(dirname "$0") && pwd)
-DEVSTACK_DIR=$TOP_DIR/devstack
+DEVSTACK_DIR=$HOME/devstack
+
+sudo pvcreate /dev/vdb
+sudo vgcreate cinder-volumes /dev/vdb
+sudo pvs
+sudo vgs
 
 git clone -b stable/juno git://git.openstack.org/openstack-dev/devstack.git $DEVSTACK_DIR
 cp local.conf $DEVSTACK_DIR/local.conf
